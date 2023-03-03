eb17bcce9e39f30beaf19a84ecad565f9ab0ced7
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-03-03 18:34:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <a
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      ></a>
     </div>
   );
 }
