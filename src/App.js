776672aa45d6075510322013647079047278acae
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-03-03 20:13:14 +0100

    Built the basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nataliia Zazerina and is
-        <a
-          href="https://github.com/Nataliia-git/my-react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="Container">
+        <Weather />
+        <footer>
+          This project was coded by Nataliia Zazerina and is{" "}
+          <a
+            href="https://github.com/Nataliia-git/my-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
