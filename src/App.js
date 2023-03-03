d4e0c67586ead54eff1de5d8de7b9c550da09b99
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-03-03 18:39:48 +0100

    Linked to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nataliia Zazerina and is
+        <a
+          href="https://github.com/Nataliia-git/my-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
