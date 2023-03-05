7f61c3fdb8c67a36e509fb351fed5329f2ba876d
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-03-05 19:23:39 +0100

    Added the CSS for Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -12,10 +12,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,10 +29,20 @@ export default function Weather() {
         <li>Friday 10:00</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Sunny" />
-          25°C
+          <div className="d-flex">
+            <div>
+              <img
+                src="http://openweathermap.org/img/wn/01d@2x.png"
+                alt="Sunny"
+              />
+            </div>
+            <div>
+              <span className="temperature">25</span>{" "}
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
